f89c4de221f9f877ee5205bdf819a9975bb319fb
KarinaDell <36238027+KarinaDell@users.noreply.github.com>
2018-06-01 08:51:22 -0500

    Add files via upload

---
+++ colas FIFO/colas FIFO/Procesador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colas_FIFO
+{
+    class Procesador
+    {
+        Proceso proceso;
+        private Proceso inicio  = null;
+        static Random rnd = new Random();
+        private int cont, vacio, completo, ultimo, ciclo;
+        private int ciclos = 300;
+
+     public void iniciarProceso()
+        {
+            ciclos = 300;
+            while(ciclos > 0)
+            {
+                atenderProceso();
+                if (inicio != null)
+                    reducirCiclos();
+                else
+                    vacio++;
+                ciclos--;
+            }
+            completo = inicio.Cuantos - 1;
+            ultimo = obtenerUltimoProceso(inicio) - completo;
+
+        }
+
+        private void atenderProceso()
+        {
+            int r;
+           for(int i = 1; i <=300; i++)
+            {
+                r = rnd.Next(1, 101);
+                if (r <= 35)
+                    proceso.agregarPrceso();
+            }
+        }
+
+       private void agregarProceso()
+        {
+            if (inicio == null)
+                inicio = proceso = new Proceso(cont++, rnd.Next(4, 15));
+            else
+                agregarProcesoRecursivo(inicio);
+        }
+
+        private void agregarProcesoRecursivo(Proceso t)
+        {
+            if (t.sig == null)
+                t.sig = proceso = new Proceso(cont++, rnd.Next(4, 15));
+            else
+                agregarProcesoRecursivo(t.sig);
+        }
+
+        }
+    }
+}
---
+++ colas FIFO/colas FIFO/Proceso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colas_FIFO
+{
+    class Proceso
+    {
+
+        private int cuantos;
+        private int duracion;
+        public Proceso sig;
+
+        public int Cuantos { get => cuantos; set => cuantos = value; }
+        public int Duracion { get => duracion; }
+
+        public Proceso (int cuantos, int duracion)
+        {
+            this.cuantos = cuantos;
+            this.duracion = duracion;
+        }
+
+        public override string ToString()
+        {
+            return "Proceso " + cuantos.ToString() + "Duracion: " + duracion;
+        }
+    }
+}
